5aea6825306427bdd25ef87a9e978b0c4f053e84
Hoegaarden-Son <hellopangx2@gmail.com>
2024-10-13 02:10:41 +0900

    domain added

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.200.85.174']
+ALLOWED_HOSTS = ['43.200.85.174', 'koreatripinfo.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
