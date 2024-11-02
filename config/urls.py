be622d07e376f2ad916710f29438f1588fdcbf13
Hoegaarden-Son <hellopangx2@gmail.com>
2024-11-03 00:58:12 +0900

    side bar added

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
+    path('a_board/', base_views.index, name='a_board'),
+    path('b_board/', base_views.index, name='b_board'),
 ]
 
 handler404 = 'common.views.page_not_found'
