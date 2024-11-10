1071f77b5ced699804c78dd3df21e01bda4e76e7
Hoegaarden-Son <hellopangx2@gmail.com>
2024-11-11 00:28:02 +0900

    Local settings added !!!

--- config/urls.py
+++ config/urls.py
@@ -14,23 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 from pybo.views import base_views
 
-from pybo import views
-
+# 기본 언어 설정이 없는 URL 패턴 (예: /admin)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', include('pybo.urls')),
-    path('common/', include('common.urls')),
+
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
+    path('pybo/', include('pybo.urls')),
+]
+
+# 언어별 URL 패턴을 포함한 i18n_patterns
+urlpatterns += i18n_patterns(
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
+    path('admin/', admin.site.urls),
+    path('pybo/', include('pybo.urls')),
+
     path('a_board/', base_views.index, name='a_board'),
     path('b_board/', base_views.index, name='b_board'),
 
-    # 다른 URL 패턴
+    path('common/', include('common.urls')),
     path('privacy/', base_views.privacy, name='privacy'),
     path('terms/', base_views.terms, name='terms'),
-]
+)
 
 handler404 = 'common.views.page_not_found'
